3c03ea5e63be9b68558685d5f10ff911d6f85d0e
ERICK RIBEIRO DE SOUZA <erick_ribeiro-souza@estudante.sesisenai.org.br>
2024-04-29 17:22:51 -0300

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+// var condicao = false;
+
+
+// var portaTres = document.querySelector(".portaTres");
+
+
+// if (condicao) {
+   
+//     portaTres.classList.add("shadow-red");
+// }else{
+//     portaTres.classList.add("shadow-green")
+// }
